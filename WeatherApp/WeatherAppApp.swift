2f836f2a4eedd402f2bec034c5815c9686b84cd2
BabyGeek <babygeek83bis@gmail.com>
2022-03-12 12:49:34 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Paul Oggero on 12/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
